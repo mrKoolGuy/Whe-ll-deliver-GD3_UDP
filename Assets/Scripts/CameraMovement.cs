901748702769eb026f58a15fd8beb7148b76aa7e
cosmo2401 <100962088+cosmo2401@users.noreply.github.com>
2023-10-19 17:07:30 +0200

    :sparkles: Added vertical movement to the camera

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,6 +8,8 @@ public class CameraMovement : MonoBehaviour
     // Update is called every frame, if the MonoBehaviour is enabled
     private void Update()
     {
-        transform.RotateAround(target.transform.position, target.transform.up, Time.deltaTime * speed * Input.GetAxis("Horizontal Camera"));
+        transform.RotateAround(target.transform.position, target.transform.up, Time.deltaTime * speed * (-Input.GetAxis("Horizontal Camera")));
+        transform.RotateAround(target.transform.position, transform.right, Time.deltaTime * speed * (-Input.GetAxis("Vertical Camera")));
+        //transform.RotateAround()
     }
 }
