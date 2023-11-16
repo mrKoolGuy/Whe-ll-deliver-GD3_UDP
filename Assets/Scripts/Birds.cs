bc01bc3fe2b90cbbe4095d60e8324d19c4480180
Moritz Hollich <moritz.hollich@mni.thm.de>
2023-11-16 23:13:18 +0000

    :bug: bird script can also handle levels now without bird spots in them

--- Assets/Scripts/Birds.cs
+++ Assets/Scripts/Birds.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -7,19 +9,23 @@ public class Birds : MonoBehaviour
     [SerializeField] private AudioClip birdsFlapping;
     
     //Selects a random tree and starts the AudioSource there.
-    //if an oldBirdSpot is given, it makes sure to select another tree
+    //It makes sure to select a tree that has no birds playing already and tries to avoid the tree the birds where just fleeing from.
     void FindNewTree(GameObject oldBirdSpot)
     {
         GameObject[] birdSpots = GameObject.FindGameObjectsWithTag("BirdSpot");
-        int numberOfBirds = birdSpots.Length;
 
-        GameObject selectedBirds;
-        do
+        if (birdSpots.Length == 1)
         {
-            selectedBirds = birdSpots[Random.Range(0, numberOfBirds)];
-        } while (numberOfBirds > 1 && selectedBirds == oldBirdSpot);
-        
-        selectedBirds.GetComponent<AudioSource>().Play();
+            //if there is only one tree in the level, continue playing the bird sounds there
+            birdSpots[0].GetComponent<AudioSource>().Play();
+        } else if (birdSpots.Length > 1)
+        {
+            List<GameObject> freeBirdSpots = birdSpots.ToList().FindAll(bs => (!bs.GetComponent<AudioSource>().isPlaying) && bs != oldBirdSpot);
+            int numberOfBirdSpots = freeBirdSpots.Count;
+
+            if (numberOfBirdSpots > 0)
+                freeBirdSpots[Random.Range(0, numberOfBirdSpots)].GetComponent<AudioSource>().Play();
+        }
     }
     
     void Start()
