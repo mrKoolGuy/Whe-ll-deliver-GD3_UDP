d9096d58e0cdbf0a405299af5a96c23b2c8e617e
cosmo2401 <100962088+cosmo2401@users.noreply.github.com>
2023-11-23 12:57:39 +0000

    Various Fixes
    Changed some player tags
    Added a new tag for groundCollison
    Fixed trampolines not working in some levels
    Changed level transition script

--- Assets/Scripts/LevelFinishReached.cs
+++ Assets/Scripts/LevelFinishReached.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class LevelFinishReached : MonoBehaviour
 {
 
-    [SerializeField] private ELevel nextLevel;
+    public UnityEvent OnLevelEnd;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            //Tell Level Manager to go to next level
+
+            OnLevelEnd?.Invoke();
+
+
             // Do somthing here when level finishes
-            string newLevel = nextLevel.ToString();
-            SceneManager.LoadScene(1);
-            Debug.Log(newLevel);
+
+            int thisLevel = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(thisLevel + 1);
+
+
+            Debug.Log("Going to level : " + (thisLevel + 1));
         }
 
     }
