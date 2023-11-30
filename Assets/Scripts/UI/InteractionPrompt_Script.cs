ceaabe1311c9227fe5ac19923e6992c26a837775
mrKoolGuy <lionelbackes@gmail.com>
2023-11-30 13:02:16 +0000

    :lipstick: Interact UI Fully Functional
    -Interact UI appears & disappears
    -Interaction UI Script has been updated
    To-Do: UI Animations need to be implemented
    Stars need to be Images

--- Assets/Scripts/UI/InteractionPrompt_Script.cs
+++ Assets/Scripts/UI/InteractionPrompt_Script.cs
@@ -6,6 +6,7 @@ public class InteractionPrompt_Script : MonoBehaviour
 {
     //[Header("Main Camera")]
     /*[SerializeField]*/ private Camera _mainCam;
+    [SerializeField] private Canvas ui_InteractPrompt;
     //[Tooltip("This is where to input the main Camera")]
 
 
@@ -22,4 +23,18 @@ public class InteractionPrompt_Script : MonoBehaviour
         var rotation = _mainCam.transform.rotation;
         transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
     }
+
+    #region ENABLE/DISABLE
+    public void Enable()
+    {
+        ui_InteractPrompt.enabled = true;
+        Debug.Log("Enabled");
+    }
+
+    public void Disable()
+    {
+        ui_InteractPrompt.enabled = false;
+        Debug.Log("Disabled");
+    }
+    #endregion
 }
