88426cf5479b87790d5ee81a2fe1047d5a19e7ba
Moritz Hollich <moritz.hollich@mni.thm.de>
2023-12-17 17:04:37 +0000

    :lipstick: Fixed UI for the Stars with new Prefab and Script - changed type of star images to sprites - removed Dialog placeholders from build settings - created Prefab for Stars - included new Star system in HUD - changed code of HUD_Script to work with the Stars script instead of doing it itself - renamed StarLost in HUD into StarsChanged, because both directions can happen - included new Star system in End Level UI

--- Assets/Scripts/UI/HUD_Script.cs
+++ Assets/Scripts/UI/HUD_Script.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class HUD_Script : MonoBehaviour
@@ -7,16 +8,12 @@ public class HUD_Script : MonoBehaviour
     #region Fields
     [Header("Progress Bar")]
     private float max = 100.0f;
-    [SerializeField] private Slider slider;
-    [Tooltip("This is the Image that is used to display the Progress Bar of the Score")]
-
-    [Header("Stars")]
-    [SerializeField] private Image star1;
-    [SerializeField] private Image star2;
-    [SerializeField] private Image star3;
-    [Tooltip("The TextMeshPro Objects for all the Stars")]
-    //[SerializeField] private Sprite fullStar;
-    //[SerializeField] private Sprite emptyStar;
+    [SerializeField] [Tooltip("This is the Image that is used to display the Progress Bar of the Score")]
+    private Slider slider;
+    
+    [Header("Stars")] [SerializeField]
+    [Tooltip("The Stars Prefab")]
+    private GameObject starsPrefab;
     #endregion
 
     #region Progress Bar
@@ -34,40 +31,9 @@ public class HUD_Script : MonoBehaviour
 
     #region Stars
     //TO-DO: Add bell sound effect
-    //To-Do: Uncomment these lines and remove the Color Changes
-    public void StarLost(int stars)
+    public void StarsChanged(int stars)
     {
-        switch (stars)
-        {
-            case 2:
-                //star3.sprite = emptyStar; 
-                star3.color = Color.grey;
-                break;
-            case 1:
-                //star2.sprite = emptyStar;
-                star2.color = Color.grey;
-                break;
-            case 0:
-                //star1.sprite = emptyStar;
-                star1.color = Color.grey;
-                break;
-        }
-            
+        starsPrefab.GetComponent<Stars>().SetStars(stars);
     }  
-    #endregion
-
-    #region TESTING
-    private void Update()
-    {
-        //ChangeFill();
-    }
-    public void DebugScoreChanged(int score)
-    {
-        Debug.Log($"Score changed to: {score}");
-    }
-    public void DebugFillAmountChanged(float fillAmount)
-    {
-        Debug.Log($"FillAmount changed to: {fillAmount}");
-    }
     #endregion
 }
