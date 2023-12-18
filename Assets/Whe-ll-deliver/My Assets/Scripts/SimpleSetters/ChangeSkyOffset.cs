b9c9f9060034b8e831ef4c37b59c1a894a4ceeda
cosmo2401 <100962088+cosmo2401@users.noreply.github.com>
2023-12-18 21:51:53 +0100

    Updated SkyOffset script to not throw errors on non linux machines

--- Assets/Whe-ll-deliver/My Assets/Scripts/SimpleSetters/ChangeSkyOffset.cs
+++ Assets/Whe-ll-deliver/My Assets/Scripts/SimpleSetters/ChangeSkyOffset.cs
@@ -17,22 +17,28 @@ public class ChangeSkyOffset : MonoBehaviour
     private void Start()
     {
         material = GetComponent<Renderer>().material;
-        
-        //this is the stored texture of the skybox, normally only accessible in the GPU
-        Texture textureGPU = material.mainTexture;
+        if (Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            //this is the stored texture of the skybox, normally only accessible in the GPU
+            Texture textureGPU = material.mainTexture;
 
-        //this creates a new texture which we can access from the CPU
-        texture2dCPU = new Texture2D(textureGPU.width, textureGPU.height, textureGPU.graphicsFormat, TextureCreationFlags.DontInitializePixels);
+            //this creates a new texture which we can access from the CPU
+            texture2dCPU = new Texture2D(textureGPU.width, textureGPU.height, textureGPU.graphicsFormat, TextureCreationFlags.DontInitializePixels);
 
-        //copy the data from the GPU into the CPU
-        Graphics.CopyTexture(textureGPU, 0, 0, 0, 0, textureGPU.width, textureGPU.height, texture2dCPU, 0, 0, 0, 0);
+            //copy the data from the GPU into the CPU
+            Graphics.CopyTexture(textureGPU, 0, 0, 0, 0, textureGPU.width, textureGPU.height, texture2dCPU, 0, 0, 0, 0);
+        }
     }
 
     private Color GetColorAtOffset(float offset)
     {
-        //the offset goes from -0.5 on the left edge to 0.5 on the right edge. The pixels go from 0 to texture2dCPU.width
-        int xPixelIndex = Math.Clamp((int)(texture2dCPU.width / 2.0 + offset * texture2dCPU.width), 0, texture2dCPU.width);
-        return texture2dCPU.GetPixel(xPixelIndex, HorizonY);
+        if (Application.platform == RuntimePlatform.LinuxPlayer)
+        {
+            //the offset goes from -0.5 on the left edge to 0.5 on the right edge. The pixels go from 0 to texture2dCPU.width
+            int xPixelIndex = Math.Clamp((int)(texture2dCPU.width / 2.0 + offset * texture2dCPU.width), 0, texture2dCPU.width);
+            return texture2dCPU.GetPixel(xPixelIndex, HorizonY);
+        }
+        return new Color(171,217,228);
     }
     
     //offset has to be between -0.5 and 0.5 and controls the look of the sky. 0 is midday, the limits in both directions are a dark sky
